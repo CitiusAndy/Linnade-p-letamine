08d030e0d62b375da97a36171941fbf3a776c1a8
Andreas Pung <andreaspung@gmail.com>
2017-03-28 18:33:46 +0300

    Arvuti vastumängimise võimaluse lisamine
    Nüüdsest saab kasutaja valida, kas ta:
    1) Mängib arvuti vastu
    2) Mängib teise mängija vastu

--- Peaklass.java
+++ Peaklass.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Peaklass {
 
@@ -12,37 +13,37 @@ public class Peaklass {
 			return 2; //kaart 2 on suurem kui kaart1
 		return 0; //võrdsed
 	}
-	
+
 	public static boolean onKorrektne(String str, int min, int max)  
 	{  
-	  try  
-	  {  
-	    double d = Integer.parseInt(str);  
-	  }  
-	  catch(NumberFormatException nfe)  
-	  {  
-		System.out.println("Palun sisesta arv!");
-	    return false;  
-	  }  
-	  
-	  //System.out.println(str + " min " +  min + " max " +  max);
-	  if(Integer.parseInt(str) < min || Integer.parseInt(str) > max){
+		try  
+		{  
+			double d = Integer.parseInt(str);  
+		}  
+		catch(NumberFormatException nfe)  
+		{  
+			System.out.println("Palun sisesta arv!");
+			return false;  
+		}  
 
-		  System.out.println();
-		  if(Double.parseDouble(str) < min){
-			  System.out.println("Arv ei tohi olla väiksem kui " + min + ".");
-			  return false;
-		  }
-		  else{
-			  System.out.println("Arv ei tohi olla suurem kui " + max + ".");
-			  return false;
-		  }
-	  }
+		//System.out.println(str + " min " +  min + " max " +  max);
+		if(Integer.parseInt(str) < min || Integer.parseInt(str) > max){
 
-	  return true;  
+			System.out.println();
+			if(Double.parseDouble(str) < min){
+				System.out.println("Arv ei tohi olla väiksem kui " + min + ".");
+				return false;
+			}
+			else{
+				System.out.println("Arv ei tohi olla suurem kui " + max + ".");
+				return false;
+			}
+		}
+
+		return true;  
 	}
 
-	public static void kaartideJarjekord(List<Kaart> kaardid, Pakk pakk, int mangijaNr, Scanner sisend) { //pakk - sinna lisatakse uued kaardid, mangijaNr - kumma mängija kaardid
+	public static void kaartideJarjekord(List<Kaart> kaardid, Pakk pakk, int mangijaNr, Scanner sisend, String manguvorm) { //pakk - sinna lisatakse uued kaardid, mangijaNr - kumma mängija kaardid
 
 		List<Kaart> kaardidKoopia =  new ArrayList<>();
 		kaardidKoopia.addAll(kaardid);
@@ -68,20 +69,34 @@ public class Peaklass {
 				if(mangijaNr == 2){
 					pakk.pakk2.add(kaardidKoopia.remove(0));
 				}
-				
+
 				break;
 			}
 
 			System.out.println();
 			System.out.println();
 			System.out.println(mangijaNr + ". mängija, palun sisesta järgmisena kaardipaki põhja lisatava kaardi järjekorranumber: ");
+
+			int indeksNumbrina;
+
+
+			if(mangijaNr == 2 && manguvorm.equals("1")){
+				Random suvaline = new Random();
+				indeksNumbrina = 1 + suvaline.nextInt(kaardidKoopia.size());
+				System.out.println("[ARVUTI]: Valisin, et järgmisena lisan kaardipaki põhja kaardi järjekorranumbriga " + indeksNumbrina + ".");
+				System.out.println();
+			}
 			
-			String indeksSonena = sisend.nextLine();
-			while(!onKorrektne(indeksSonena, 1, kaardidKoopia.size())){
-				indeksSonena = sisend.nextLine();
+			else{
+				String indeksSonena = sisend.nextLine();
+				while(!onKorrektne(indeksSonena, 1, kaardidKoopia.size())){
+					indeksSonena = sisend.nextLine();
+				}
+
+				indeksNumbrina = Integer.parseInt(indeksSonena);
 			}
 
-			int indeksNumbrina = Integer.parseInt(indeksSonena);
+
 			if(mangijaNr == 1){
 				pakk.pakk1.add(kaardidKoopia.remove(indeksNumbrina - 1));
 			}
@@ -91,30 +106,30 @@ public class Peaklass {
 
 			System.out.println("Mängija 1 pakk peale kaardi lisamist: " + pakk.valjastaPakk1() + pakk.pakk1.size());
 			System.out.println("Mängija 2 pakk peale kaardi lisamist: " + pakk.valjastaPakk2() + pakk.pakk2.size());
-			
-//			System.out.println(i);
+
+			//			System.out.println(i);
 		}
 
 	}
-	
-	public static void nimi(int kaartideVordlus, Kaart kaart1, Kaart kaart2, Pakk pakk, Scanner sisend, List<Kaart> voidetavadKaardid, List<Kaart> mangija1Kaardid, List<Kaart> mangija2Kaardid) {
+
+	public static void kaartidePohjaLisamine(int kaartideVordlus, Kaart kaart1, Kaart kaart2, Pakk pakk, Scanner sisend, List<Kaart> voidetavadKaardid, List<Kaart> mangija1Kaardid, List<Kaart> mangija2Kaardid, String manguvorm) {
 		if(kaartideVordlus == 1){
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 1, sisend);
-			kaartideJarjekord(voidetavadKaardid, pakk, 1, sisend);
+			kaartideJarjekord(voidetavadKaardid, pakk, 1, sisend, manguvorm);
 		}
 		if(kaartideVordlus == 2){
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 2, sisend);
-			kaartideJarjekord(voidetavadKaardid, pakk, 2, sisend);
+			kaartideJarjekord(voidetavadKaardid, pakk, 2, sisend, manguvorm);
 		}
 		if(kaartideVordlus == 0){
-			
+
 			//System.out.println("võrdsed");
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 0);
 			List<Kaart> kaardid1 = new ArrayList<>();
 			List<Kaart> kaardid2 = new ArrayList<>();
 			kaardid1.add(kaart1);
 			kaardid2.add(kaart2);
-			
+
 			if(pakk.pakk1.size() == 0){
 				pakk.pakk1.add(kaardid1.remove(0));
 			}
@@ -125,7 +140,7 @@ public class Peaklass {
 			Kaart tagurpidiKaart2 = pakk.votaKaart2();
 			kaardid1.add(tagurpidiKaart1);
 			kaardid2.add(tagurpidiKaart2);
-			
+
 			if(pakk.pakk1.size() == 0){
 				pakk.pakk1.add(kaardid1.remove(0));
 			}
@@ -136,19 +151,21 @@ public class Peaklass {
 			Kaart uusKaart2 = pakk.votaKaart2();
 			kaardid1.add(uusKaart1);
 			kaardid2.add(uusKaart2);
-			
+
 			List<Kaart> kokkuKaardid = new ArrayList<>();
 			kokkuKaardid.addAll(kaardid1);
 			kokkuKaardid.addAll(kaardid2);
-			
-			nimi(vordleKaarte(uusKaart1, uusKaart2), uusKaart1, uusKaart2, pakk, sisend, kokkuKaardid, kaardid1, kaardid2);
+
+			kaartidePohjaLisamine(vordleKaarte(uusKaart1, uusKaart2), uusKaart1, uusKaart2, pakk, sisend, kokkuKaardid, kaardid1, kaardid2, manguvorm);
 		}
 	}
 
 
-	public static void mangi() {
+	public static void mangi(String manguvorm) {
+
+
 		Scanner sisend = new Scanner(System.in);
-		
+
 		Pakk pakk = new Pakk();
 		System.out.println("Segamata tervikpakk: " + pakk.valjastaKaardid());
 		pakk.sega();
@@ -179,13 +196,13 @@ public class Peaklass {
 			if(kaart1.getTugevusarv() == kaart2.getTugevusarv()){
 				System.out.println(", kaardid osutusid tugevuselt võrdseteks!");
 			}
-			
+
 			System.out.println();
-			
+
 			int kaartideVordlus = vordleKaarte(kaart1, kaart2);
 
-			nimi(kaartideVordlus, kaart1, kaart2, pakk, sisend, Arrays.asList(kaart1, kaart2), Arrays.asList(kaart1), Arrays.asList(kaart2));
-			
+			kaartidePohjaLisamine(kaartideVordlus, kaart1, kaart2, pakk, sisend, Arrays.asList(kaart1, kaart2), Arrays.asList(kaart1), Arrays.asList(kaart2), manguvorm);
+
 			System.out.println();
 			System.out.println("Mängija 1 pakk peale kaardi lisamist: " + pakk.valjastaPakk1() + pakk.pakk1.size());
 			System.out.println("Mängija 2 pakk peale kaardi lisamist: " + pakk.valjastaPakk2() + pakk.pakk2.size());
@@ -198,7 +215,18 @@ public class Peaklass {
 
 	public static void main(String[] args) {
 		System.out.println("Linnade põletamise mäng!");
-		mangi();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Kas te soovite mängida arvuti vastu (1) või kahekesi (2)?");
+		
+
+		String manguvorm = scan.nextLine();
+		while(!onKorrektne(manguvorm, 1, 2))
+			manguvorm = scan.nextLine();
+		
+		if(manguvorm.equals("1"))
+			System.out.println("Nüüdsest mängite arvuti vastu!");
+
+		mangi(manguvorm);
 
 	}
 
