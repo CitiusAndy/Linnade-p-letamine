8b5360d429e9f499cbd8f0f5aee5f290fe01f79c
Andreas Pung <andreaspung@gmail.com>
2017-05-09 18:46:32 +0300

    GUI alustamine
    Õige GUI alustamine, avaaken töötab.

--- Graafika.java
+++ Graafika.java
@@ -6,18 +6,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
@@ -64,16 +63,16 @@ public class Graafika extends Application {
 		return true;  
 	}
 
-	public static void kaartideJarjekord(List<Kaart> kaardid, Pakk pakk, int mangijaNr, Scanner sisend, String manguvorm, 
-			Stage peaLava, BorderPane piir2) { //pakk - sinna lisatakse uued kaardid, mangijaNr - kumma mängija kaardid
+	public static void kaartideJarjekord(List<Kaart> kaardid, Pakk pakk, int mangijaNr, String manguvorm, 
+			Stage peaLava) { //pakk - sinna lisatakse uued kaardid, mangijaNr - kumma mängija kaardid
 
+		/*
 		Button nupp1 = new Button();
 		piir2.setLeft(nupp1);
 		Button nupp2 = new Button();
 		piir2.setRight(nupp2);
+		 */
 
-
-	
 		List<Kaart> kaardidKoopia =  new ArrayList<>();
 		kaardidKoopia.addAll(kaardid);
 		for(int i = 0; i < kaardid.size(); i++) {
@@ -111,13 +110,13 @@ public class Graafika extends Application {
 			}
 
 			else{ //Kasutaja sisestamine
-				
-				
-				String indeksSonena = sisend.nextLine();
 
 
+				//String indeksSonena = sisend.nextLine();
+
+				/*
 				while(!onKorrektne(indeksSonena, 1, kaardidKoopia.size())){
-					indeksSonena = sisend.nextLine();
+					//indeksSonena = sisend.nextLine();
 				}
 
 				indeksNumbrina = Integer.parseInt(indeksSonena);
@@ -130,32 +129,29 @@ public class Graafika extends Application {
 			if(mangijaNr == 2){
 				pakk.pakk2.add(kaardidKoopia.remove(indeksNumbrina - 1));
 			}
+				 */
 
-			Scene mang = new Scene(piir2);
-			peaLava.setScene(mang);
-			peaLava.show();
 
-			System.out.println("Mängija 1 pakk peale kaardi lisamist: " + pakk.valjastaPakk1() + pakk.pakk1.size());
-			System.out.println("Mängija 2 pakk peale kaardi lisamist: " + pakk.valjastaPakk2() + pakk.pakk2.size());
+				System.out.println("Mängija 1 pakk peale kaardi lisamist: " + pakk.valjastaPakk1() + pakk.pakk1.size());
+				System.out.println("Mängija 2 pakk peale kaardi lisamist: " + pakk.valjastaPakk2() + pakk.pakk2.size());
 
+			}
 		}
-
 	}
 
-	public static void kaartidePohjaLisamine(int kaartideVordlus, Kaart kaart1, Kaart kaart2, Pakk pakk, 
-			Scanner sisend, List<Kaart> voidetavadKaardid, List<Kaart> mangija1Kaardid, 
-			List<Kaart> mangija2Kaardid, String manguvorm, Stage peaLava, BorderPane piir2) {
+	public static void kaartidePohjaLisamine(int kaartideVordlus, Kaart kaart1, Kaart kaart2, Pakk pakk, List<Kaart> voidetavadKaardid, List<Kaart> mangija1Kaardid, 
+			List<Kaart> mangija2Kaardid, String manguvorm, Stage peaLava) {
+
 		if(kaartideVordlus == 1) {
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 1, sisend);
-			kaartideJarjekord(voidetavadKaardid, pakk, 1, sisend, manguvorm, peaLava, piir2);
+			kaartideJarjekord(voidetavadKaardid, pakk, 1, manguvorm, peaLava);
 		}
 		if(kaartideVordlus == 2) {
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 2, sisend);
-			kaartideJarjekord(voidetavadKaardid, pakk, 2, sisend, manguvorm, peaLava, piir2);
+			kaartideJarjekord(voidetavadKaardid, pakk, 2, manguvorm, peaLava);
 		}
 		if(kaartideVordlus == 0) {
 
-			//System.out.println("võrdsed");
 			//kaartideJarjekord(Arrays.asList(kaart1, kaart2), pakk, 0);
 			List<Kaart> kaardid1 = new ArrayList<>();
 			List<Kaart> kaardid2 = new ArrayList<>();
@@ -190,31 +186,27 @@ public class Graafika extends Application {
 			kokkuKaardid.addAll(kaardid1);
 			kokkuKaardid.addAll(kaardid2);
 
-			kaartidePohjaLisamine(vordleKaarte(uusKaart1, uusKaart2), uusKaart1, uusKaart2, pakk, sisend, kokkuKaardid, 
-					kaardid1, kaardid2, manguvorm, peaLava, piir2);
+			kaartidePohjaLisamine(vordleKaarte(uusKaart1, uusKaart2), uusKaart1, uusKaart2, pakk, kokkuKaardid, 
+					kaardid1, kaardid2, manguvorm, peaLava);
 		}
 	}
 
 
 	public static void mangi(String manguvorm, Stage peaLava) {
 
-		// teine piiripaan, mis pannakse esimesele piiripaanile alla
-		// teisele piiripaanile tuleb 2 nuppu, üks vasakule ja
-		// üks paremale
-		BorderPane piir2 = new BorderPane();
-		VBox kusimused = new VBox();
-		Label mangija1 = new Label();
-		Label mangija2 = new Label();
-		Label kelle = new Label();
-		kusimused.getChildren().addAll(mangija1, mangija2, kelle);
-		piir2.setTop(kusimused);
+		Group juur = new Group();
+		Canvas louend = new Canvas();
+		GraphicsContext sisu = louend.getGraphicsContext2D(); 
+		
+		sisu.fillRect(20, 30,  30, 20);
+		juur.getChildren().add(louend);
+		
+		Scene stseen = new Scene(juur, 600, 300, Color.DARKGREEN);
+		
+		peaLava.setTitle("Linnade põletamine");
+		peaLava.setScene(stseen);
+		peaLava.show();
 
-		Button nupp1 = new Button();
-		piir2.setLeft(nupp1);
-		Button nupp2 = new Button();
-		piir2.setRight(nupp2);
-
-		Scanner sisend = new Scanner(System.in);
 
 		Pakk pakk = new Pakk();
 		System.out.println("Segamata tervikpakk: " + pakk.valjastaKaardid());
@@ -222,10 +214,10 @@ public class Graafika extends Application {
 		System.out.println("Segatud tervikpakk: " + pakk.valjastaKaardid());
 		pakk.poolita();
 		System.out.println("Mängija 1 esialgne pakk: " + pakk.valjastaPakk1() + pakk.pakk1.size());
-		mangija1.setText(pakk.valjastaPakk1() + pakk.pakk1.size());
+
 
 		System.out.println("Mängija 2 esialgne pakk: " + pakk.valjastaPakk2() + pakk.pakk2.size());
-		mangija2.setText(pakk.valjastaPakk2() + pakk.pakk2.size());
+
 
 
 
@@ -242,32 +234,27 @@ public class Graafika extends Application {
 			Kaart kaart2 = pakk.votaKaart2();
 			System.out.println();
 			System.out.print("Esimene mängija käis kaardi: " + kaart1 + " ning teine mängija: " + kaart2); //VÄLJATRÜKK
-			nupp1.setText(kaart1.toString());
-			nupp2.setText(kaart2.toString());
+
 
 
 			if(kaart1.getTugevusarv() > kaart2.getTugevusarv()) {
 				System.out.println(", esimene mängija saab kaardid endale.");
-				kelle.setText("Esimene mängija saab kaardid endale.");
 			}
 			if(kaart1.getTugevusarv() < kaart2.getTugevusarv()) {
 				System.out.println(", teine mängija saab kaardid endale.");
-				kelle.setText("teine mängija saab kaardid endale.");
+
 			}
 			if(kaart1.getTugevusarv() == kaart2.getTugevusarv()) {
 				System.out.println(", kaardid osutusid tugevuselt võrdseteks!");
-				kelle.setText("kaardid osutusid tugevuselt võrdseteks!");
 			}
 
-			Scene mang = new Scene(piir2);
-			peaLava.setScene(mang);
-			peaLava.show();
+
 
 			System.out.println();
 
 			int kaartideVordlus = vordleKaarte(kaart1, kaart2);
 
-			kaartidePohjaLisamine(kaartideVordlus, kaart1, kaart2, pakk, sisend, Arrays.asList(kaart1, kaart2), Arrays.asList(kaart1), Arrays.asList(kaart2), manguvorm, peaLava, piir2);
+			kaartidePohjaLisamine(kaartideVordlus, kaart1, kaart2, pakk, Arrays.asList(kaart1, kaart2), Arrays.asList(kaart1), Arrays.asList(kaart2), manguvorm, peaLava);
 
 			System.out.println();
 			System.out.println("Mängija 1 pakk peale kaardi lisamist: " + pakk.valjastaPakk1() + pakk.pakk1.size());
@@ -275,69 +262,78 @@ public class Graafika extends Application {
 			System.out.println();
 
 		}
-		sisend.close();
 
 	}
 
 	@Override
 	public void start(Stage peaLava) {
 
-		BorderPane piir = new BorderPane();
+		// Luuakse hüpikakna jaoks teine lava
+		Stage kusimus = new Stage();
 
-		// aknasündmuse lisamine
-		peaLava.setOnShown(new EventHandler<WindowEvent>() {
+		// Küsimuse ja kolme nupu loomine
+		Label label = new Label("Kelle vastu tahad mängida?");
+		Button arvuti = new Button("Arvuti");
+		Button mangija = new Button("Mängija");
+		Button valju = new Button("Välju");
+
+		// Sündmuse lisamine nupule arvuti
+		arvuti.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-			public void handle(WindowEvent event) {
-
-				// luuakse teine lava
-				Stage kusimus = new Stage();
-				// küsimuse ja kahe nupu loomine
-				Label label = new Label("Kelle vastu tahad mängida?");
-				Button arvuti = new Button("Arvuti");
-				Button mangija = new Button("Mängija");
-
-				// sündmuse lisamine nupule arvuti
-				arvuti.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						kusimus.hide();
-						mangi("1", peaLava);
-					}
-				});
-
-				// sündmuse lisamine nupule mängija
-				mangija.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						kusimus.hide();
-						mangi("2", peaLava);
-					}
-				});
-
-				// nuppude grupeerimine
-				FlowPane pane = new FlowPane(10, 10);
-				pane.setAlignment(Pos.CENTER);
-				pane.getChildren().addAll(mangija, arvuti);
-
-				// küsimuse ja nuppude gruppi paigutamine
-				VBox vBox = new VBox(10);
-				vBox.setAlignment(Pos.CENTER);
-				vBox.getChildren().addAll(label, pane);
-
-				//stseeni loomine ja näitamine
-				Scene stseen2 = new Scene(vBox);
-				kusimus.setScene(stseen2);
-				kusimus.show();
+			public void handle(ActionEvent event) {
+				kusimus.hide();
+				mangi("1", peaLava);
 			}
-		}); //siin lõpeb aknasündmuse kirjeldus
+		});
+
+		// Sündmuse lisamine nupule mängija
+		mangija.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				kusimus.hide();
+				mangi("2", peaLava);
+			}
+		});
+
+		// Sündmuse lisamine nupule valju
+		valju.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Platform.exit(); //Mäng läheb kinni
+			}
+		});
 
 
-		// stseeni loomine ja näitamine
-		Scene stseen1 = new Scene(piir, 300, 150, Color.SNOW);
+		// Nuppude grupeerimine
+		FlowPane pane = new FlowPane(10, 20); //Üleval vahe 10px, kõrvalt vahe 20px
+		pane.setAlignment(Pos.CENTER);
+		pane.getChildren().addAll(mangija, arvuti, valju);
+
+		// Küsimuse ja nuppude vertikaalselt paigutamine
+		VBox vBox = new VBox(20);
+		vBox.setAlignment(Pos.CENTER);
+		vBox.getChildren().addAll(label, pane);
+
+		// Stseeni loomine ja näitamine
+		Scene hupikStseen = new Scene(vBox);
+		kusimus.setScene(hupikStseen);
+		kusimus.setTitle("Vastase valimine");
+		kusimus.show();
+
+
+		//Hüpikakna taga oleva akna loomine
+		/*
+		Group juur = new Group();
+		Canvas louend = new Canvas();
+		GraphicsContext sisu = louend.getGraphicsContext2D(); 
+		juur.getChildren().add(louend);
+		Scene avaStseen = new Scene(juur, 600, 300, Color.DARKGREEN);
 		peaLava.setTitle("Linnade põletamine");
-		peaLava.setResizable(false);
-		peaLava.setScene(stseen1);
-		peaLava.show();
+		peaLava.setScene(avaStseen);
+		 */
+		
+		//peaLava.show();
+
 	}
 
 	public static void main(String[] args) {
